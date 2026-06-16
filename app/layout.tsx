6f08d0462545ef02bb710b74a81d5282491aac96
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-06-16 04:17:22 -1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-background dark">
       <head>
-       <script src="//code.jivosite.com/widget/0fjHk31MUi" async></script>
+ <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '7b4841efa688aedfe6f7e1fa634e5dac501b905b';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
 
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased bg-black text-white`}>
